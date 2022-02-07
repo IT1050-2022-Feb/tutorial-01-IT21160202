e0062fc77738b479318e538f65edff1344eb6cb7
Mr. IT21160202 <99157095+IT21160202@users.noreply.github.com>
2022-02-07 06:19:06 +0000

    completed tute03.c

--- Tute03.c
+++ Tute03.c
@@ -8,12 +8,26 @@ e.g.
 n -> 100
 sum = 1+2+3+....+ 99+100 = 5050
 
-n -> 1-
+n -> 10
 sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
-  
-  return 0;
+	// variable declaration
+	int n, sum = 0;
+
+	// get user input
+	printf("Enter the number: ");
+	scanf("%d", &n);
+
+	// calculate sum
+	for(int i = 0; i <=n; i++){
+		sum += i;
+	}
+
+	// print sum
+	printf("sum = %d", sum);
+
+	return 0;
 }
 
